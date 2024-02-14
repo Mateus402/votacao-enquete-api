d80e0e822b707649b932108f4c0f623225ce49de
Mateus Stangherlin <mateus_stangherlin@hotmail.com>
2024-02-14 11:42:32 -0300

    Feat | Include a Pattern Pub Sub Voting

--- src/http/ws/poll-results.ts
+++ src/http/ws/poll-results.ts
@@ -3,7 +3,7 @@ import { FastifyInstance } from "fastify";
 export async function pollResults(app: FastifyInstance) {
     app.get('/polls/:pollId/results', { websocket: true }, (connection, request) => {
         connection.socket.on('messagem', (message: string)=> {
-            connection.socket.send('you sent: ' + message)
+            
         })
     })
 }
